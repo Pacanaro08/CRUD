289c255d47ef261369664ca2f3d9758aadccb438
Pietro Pacanaro <pietro.macho12@gmail.com>
2024-09-03 21:15:55 -0300

    center everything and create buttons

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class Window:
         self.frame()
         self.label()
         self.text()
+        self.button()
     
     def config(self) -> None:
         """pattern configuration"""
@@ -25,45 +26,74 @@ class Window:
     def frame(self) -> None:
         """frame building"""
 
-        self.main_window = Frame(self.master)
-        self.main_window.pack()
+        self.master.grid_columnconfigure(0, weight=1)
 
-        self.a = Frame(self.master)
-        self.a.pack()
+        self.main_window = Frame(self.master)
+        self.main_window.grid(row=0, column=0, columnspan=2, pady=(20, 10), padx=10, sticky="nsew")
+
+        self.email_frame = Frame(self.master)
+        self.email_frame.grid(row=1, column=0, columnspan=2, pady=(20, 10), padx=10, sticky="nsew")
+
+        self.password_frame = Frame(self.master)
+        self.password_frame.grid(row=2, column=0, columnspan=2, pady=(10, 20), padx=10, sticky="nsew")
+
+        self.action_frame = Frame(self.master)
+        self.action_frame.grid(row=3, column=0, columnspan=2, pady=(10, 20), padx=10, sticky="nsew")
+
+
+        self.main_window.grid_columnconfigure(0, weight=1)
+        self.email_frame.grid_columnconfigure(0, weight=0)
+        self.email_frame.grid_columnconfigure(1, weight=1)
+        self.password_frame.grid_columnconfigure(0, weight=0)
+        self.password_frame.grid_columnconfigure(1, weight=1)
+        self.action_frame.grid_columnconfigure(0, weight=0)
+        self.action_frame.grid_columnconfigure(1, weight=0)
+    
     
     def label(self) -> None:
         """label building"""
 
         self.title = Label(self.main_window, text="Welcome!")
         self.title["font"] = self.title_font
-        self.title.pack()
+        self.title.grid(row=0, column=0, pady=(10, 0), padx=(10, 0), columnspan=2, sticky="n")
 
         self.subtitle = Label(self.main_window, text="Please, Log in")
-        self.subtitle["pady"] = 10
         self.subtitle["font"] = self.subtitle_font
-        self.subtitle.pack()
+        self.subtitle.grid(row=1, column=0, pady=(10, 20), padx=(10, 0), columnspan=2, sticky="n")
 
-        self.email_label = Label(self.main_window, text="E-mail:")
-        self.email_label["pady"] = 20
-        self.email_label["padx"] = 5
+        self.email_label = Label(self.email_frame, text="E-mail:")
         self.email_label["font"] = self.text_font
-        self.email_label.pack(side=LEFT)
+        self.email_label.grid(row=0, column=0, padx=(100,0), sticky="w")
 
-        self.password_label = Label(self.a, text="Password:")
-        self.password_label["padx"] = 5
+        self.password_label = Label(self.password_frame, text="Password:")
         self.password_label["font"] = self.text_font
-        self.password_label.pack(side=LEFT)
+        self.password_label.grid(row=0, column=0, padx=(100,0), sticky="w")
+
     
     def text(self) -> None:
         """text building"""
 
-        self.email_text = Text(self.main_window, height=1, width=50)
-        self.email_text.pack(side=LEFT)
+        self.email_text = Entry(self.email_frame)
+        self.email_text.grid(row=0, column=1, sticky="we", padx=(5, 100))
 
-        self.password_text = Entry(self.a)
+        self.password_text = Entry(self.password_frame)
         self.password_text.configure(show="*")
-        self.password_text.pack(side=LEFT)
+        self.password_text.grid(row=0, column=1, sticky="we", padx=(5, 100))
 
+    def button(self) -> None:
+        """button building"""
+
+        self.login = Button(self.action_frame, text="Log In", command=self.actionLogIn, width=25, activebackground="gray", activeforeground="white")
+        self.login.grid(row=0, column=0, padx=(100, 5))
+
+        self.register = Button(self.action_frame, text="Register", command=self.registerIn, width=25, activebackground="gray", activeforeground="white")
+        self.register.grid(row=0, column=1, padx=(5,100))
+
+    def actionLogIn(self) -> None:
+        print("try login")
+
+    def registerIn(self) -> None:
+        print("try register")
 
 
 root = Tk()
